2e415ca81c737f6333803d2eda1a64ae112cfeb4
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2021-03-02 03:04:24 +0300

    +add Character, CharacterController, ICharacterController, CharacterAnimationController

---
+++ Assets/[GAME]/Scripts/Character/CharacterController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour, ICharacterController
+{
+    [SerializeField] private float _moveSpeedForward;
+    [SerializeField] private float _xPositionLimit;
+    [SerializeField] private bool _isRunning;
+    public bool IsRunning { get { return _isRunning; } set { _isRunning = value; } }
+
+    private void OnEnable()
+    {
+        if (Managers.Instance == null)
+            return;
+        EventManager.OnSwipeDetected.AddListener(Surf);
+        EventManager.OnLevelStart.AddListener(() => IsRunning = true);
+        EventManager.OnLevelFinish.AddListener(() => IsRunning = false);
+    }
+
+    private void OnDisable()
+    {
+        if (Managers.Instance == null)
+            return;
+        EventManager.OnSwipeDetected.RemoveListener(Surf);
+        EventManager.OnLevelStart.RemoveListener(() => IsRunning = true);
+        EventManager.OnLevelFinish.RemoveListener(() => IsRunning = false);
+    }
+
+    private void Update()
+    {
+        if (!IsRunning)
+            return;
+        Run();
+    }
+
+    public void Run()
+    {
+        transform.position += Vector3.forward * _moveSpeedForward * Time.deltaTime;
+    }
+
+    public void Surf(float xPosition)
+    {
+        transform.position += Vector3.right * xPosition * Time.deltaTime;
+        Clamp();
+    }
+
+    private void Clamp()
+    {
+        if (transform.position.x < -_xPositionLimit)
+            transform.position = new Vector3(-_xPositionLimit, transform.position.y, transform.position.z);
+        else if (transform.position.x > _xPositionLimit)
+            transform.position = new Vector3(_xPositionLimit, transform.position.y, transform.position.z);
+    }
+}
