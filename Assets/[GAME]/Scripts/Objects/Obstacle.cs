2fcca7e672864ff70421e8c4405ffd2f24f7ad85
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2021-03-02 03:02:40 +0300

    +add Cylinder, Obstacle, HumanoidCharacter, MultiplierPlatform

---
+++ Assets/[GAME]/Scripts/Objects/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        ISlammable slammable = other.GetComponent<ISlammable>();
+        if (slammable != null)
+        {
+            slammable.Slam();
+        }
+    }
+}
